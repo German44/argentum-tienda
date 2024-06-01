8acc2dd6cc91cb3a8bf1eacf4f943c87a0584385
German <g44ravarotto@gmail.com>
2024-06-01 15:42:05 -0300

    add navbar

--- ArgentumTienda/src/App.jsx
+++ ArgentumTienda/src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import BannerTop from "./components/bannerTop/BannerTop";
 import Carousel from "./components/carousel/Carousel";
+import NavBar from "./components/navBar/NavBar";
 
 function App() {
   return (
     <>
       <div className="w-full min-h-screen">
         <BannerTop />
+        <NavBar />
       </div>
     </>
   );
---
+++ ArgentumTienda/src/components/logo/Logo.jsx
@@ -0,0 +1,13 @@
+import LOGO from "../../assets/LOGO.jpg";
+
+const Logo = () => {
+  return (
+    <>
+      <img 
+      className="h-8 w-8 sm:w-10 sm:h-10"
+      src={LOGO} alt="Logo"/>
+    </>
+  );
+};
+
+export default Logo;
