f936152281a669eb7c4d27090d5bf0aa834b9463
German Ravarotto <g44ravarotto@gmail.com>
2024-06-03 11:23:43 -0300

    routes

---
+++ ArgentumTienda/src/layout/HomeLayout.jsx
@@ -0,0 +1,10 @@
+import Home from "../pages/home/Home"
+
+
+const HomeLayout = () => {
+  return (
+    <div><Home/></div>
+  )
+}
+
+export default HomeLayout
---
+++ ArgentumTienda/src/routes/Routes.jsx
@@ -0,0 +1,8 @@
+import { createBrowserRouter } from "react-router-dom";
+import 
+export const routes = createBrowserRouter ([
+    {
+        path: "/",
+        element: <HomeLayout />,
+    }
+])
